9760be434bd4a39cfebe477d648fb2d259858bea
Isaac Obuya <Szack4477@gmail.com>
2019-12-18 16:41:01 +0300

    setup and test development environment for the project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HomePage = () => (
+  <div>
+    <p>Hello there</p>
+  </div>
+);
+
+ReactDOM.render(<HomePage />, document.getElementById("app"));
