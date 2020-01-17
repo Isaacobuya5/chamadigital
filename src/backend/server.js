5ba07f3e7d1e9cede228ca2fa3571e498e29b4ce
Isaac Obuya <Szack4477@gmail.com>
2020-01-17 12:27:47 +0300

    fix favico issue

--- src/backend/server.js
+++ src/backend/server.js
@@ -27,7 +27,7 @@ mongoose
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
